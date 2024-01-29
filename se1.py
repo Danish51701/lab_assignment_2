96826f0ccb0256f0849ff625d1e9fc4cb4b8290a
Danish <md481@snu.edu.in>
2024-01-29 20:53:08 +0530

    git staus

--- se1.py
+++ se1.py
@@ -14,7 +14,8 @@ class EmployeeTable:
 
     def display_table(self):
         for emp in self.employees:
-            print("{emp.emp_id}\t{emp.name}\t{emp.age}\t{emp.salary}")
+            print("{}\t{}\t{}\t{}".format(emp.emp_id, emp.name, emp.age, emp.salary))
+
 
     def sort_table(self, key):
         if key == 1:  # Sort by Age
@@ -26,7 +27,6 @@ class EmployeeTable:
         else:
             print("Invalid sorting parameter")
 
-# Sample data
 employee1 = Employee("161E90", "Ramu", 35, 59000)
 employee2 = Employee("171E22", "Tejas", 30, 82100)
 employee3 = Employee("171G55", "Abhi", 25, 100000)
@@ -39,10 +39,11 @@ employee_table.add_employee(employee2)
 employee_table.add_employee(employee3)
 employee_table.add_employee(employee4)
 
-# User input for sorting parameter
+# User choose for sorting parameter
 sorting_option = int(input("Choose sorting parameter (1. Age, 2. Name, 3. Salary): "))
 employee_table.sort_table(sorting_option)
 
 # Display sorted table
 print("\nSorted Employee Table:")
+print("Emp_ID\tName\tAge\tSalary(PM)")
 employee_table.display_table()
